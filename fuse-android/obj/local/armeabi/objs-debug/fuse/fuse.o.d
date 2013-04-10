307b195173468115aa91e4041aa553f0e06547e9
Lucas Pizzagalli <luckpizza@gmail.com>
2013-04-10 15:09:35 +0200

    Adding fuse-android and generalizing paths

---
+++ fuse-android/obj/local/armeabi/objs-debug/fuse/fuse.o.d
@@ -0,0 +1,241 @@
+obj/local/armeabi/objs-debug/fuse/fuse.o: jni/fuse.c jni/fuse_i.h \
+ jni/include/fuse.h jni/include/fuse_common.h jni/include/fuse_opt.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/fcntl.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/fcntl.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/fcntl.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/fcntl.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/unistd.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/select.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/signal.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/limits.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/string.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/signal.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/sysconf.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/capability.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/pathconf.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/time.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/utime.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/utime.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/stat.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/stat.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/endian.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/endian.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/endian.h \
+ jni/include/sys/statvfs.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/uio.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/uio.h \
+ jni/include/fuse_lowlevel.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/pthread.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sched.h \
+ jni/include/fuse_opt.h jni/fuse_misc.h jni/include/config.h \
+ jni/include/fuse_common_compat.h jni/include/fuse_compat.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/statfs.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/vfs.h \
+ jni/include/fuse_kernel.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdio.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/alloca.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/strings.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/memory.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/errno.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/errno.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/errno.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/dlfcn.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/assert.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/param.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/param.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/param.h
+
+jni/fuse_i.h:
+
+jni/include/fuse.h:
+
+jni/include/fuse_common.h:
+
+jni/include/fuse_opt.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/fcntl.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/fcntl.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/fcntl.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/unistd.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/select.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/signal.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/string.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/signal.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/sysconf.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/capability.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/pathconf.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/time.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/utime.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/utime.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/stat.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/stat.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/endian.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/endian.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/endian.h:
+
+jni/include/sys/statvfs.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/uio.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/uio.h:
+
+jni/include/fuse_lowlevel.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/pthread.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sched.h:
+
+jni/include/fuse_opt.h:
+
+jni/fuse_misc.h:
+
+jni/include/config.h:
+
+jni/include/fuse_common_compat.h:
+
+jni/include/fuse_compat.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/statfs.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/vfs.h:
+
+jni/include/fuse_kernel.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/strings.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/memory.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/errno.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/errno.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/errno.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/dlfcn.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/assert.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/param.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/param.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/param.h:
---
+++ fuse-android/obj/local/armeabi/objs-debug/fuse/ulockmgr.o.d
@@ -0,0 +1,220 @@
+obj/local/armeabi/objs-debug/fuse/ulockmgr.o: jni/ulockmgr.c \
+ jni/include/ulockmgr.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/fcntl.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/fcntl.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/fcntl.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/fcntl.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/unistd.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/select.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/signal.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/limits.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/string.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/signal.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/sysconf.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/capability.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/pathconf.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdio.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/alloca.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/strings.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/memory.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/pthread.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/time.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sched.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/errno.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/errno.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/errno.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/assert.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/stat.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/stat.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/endian.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/endian.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/endian.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/socket.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/socket.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/socket.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/sockios.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/sockios.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/uio.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/wait.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/resource.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/resource.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/resource.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/resource.h \
+ /home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/wait.h
+
+jni/include/ulockmgr.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/fcntl.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/fcntl.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/fcntl.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/unistd.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/select.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/signal.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/string.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/signal.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/sysconf.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/capability.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/pathconf.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/strings.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/memory.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/pthread.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/time.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sched.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/errno.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/errno.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/errno.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/assert.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/stat.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/stat.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/endian.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/endian.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/machine/endian.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/socket.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/socket.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/socket.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/sockios.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/sockios.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/uio.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/wait.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/resource.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/resource.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm/resource.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/asm-generic/resource.h:
+
+/home/lpizzagalli/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/linux/wait.h:
